9e324c319ad327bb7e9d3c72319d9339d2f79e45
Christoph Deil <Deil.Christoph@gmail.com>
2017-07-24 14:50:33 +0200

    update

---
+++ tile_orientation/README.md
@@ -0,0 +1,10 @@
+# Demonstration of HiPS tile orientation
+
+This script and images show that both HiPS tiles and HiPS all-sky files 
+are up-down flipped in FITS format when compared to the JPEG and PNG format.
+
+![Allsky.jpg](https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.jpg "Original all-sky image in JPG format")
+
+![Allsky_from_JPG.jpg](Allsky_from_JPG.jpg "An all-sky image in JPG format")
+
+![Allsky_from_FITS.jpg](Allsky_from_FITS.jpg "An all-sky image in FITS format")
---
+++ tile_orientation/tile_orientation.py
@@ -0,0 +1,32 @@
+import matplotlib
+matplotlib.use('agg')
+from hips import HipsTileMeta, HipsTileAllskyArray
+import matplotlib.pyplot as plt
+from astropy.visualization.mpl_normalize import simple_norm
+
+meta_jpg = HipsTileMeta(order=3, ipix=497, file_format='jpg')
+url = 'https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.jpg'
+allsky = HipsTileAllskyArray.fetch(meta_jpg, url)
+data = allsky.data.astype('float')
+norm = simple_norm(data, 'linear', min_cut=0, max_cut=data.max())
+plt.figure()
+plt.imshow(data, origin='lower', norm=norm)
+plt.colorbar()
+plt.savefig('Allsky_from_JPG.jpg')
+
+meta_fits = HipsTileMeta(order=3, ipix=497, file_format='fits')
+url = 'https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.fits'
+allsky = HipsTileAllskyArray.fetch(meta_fits, url)
+data = allsky.data.astype('float')
+norm = simple_norm(data, 'log', min_cut=0, max_cut=data.max())
+plt.figure()
+plt.imshow(data, origin='lower', norm=norm)
+plt.colorbar()
+plt.savefig('Allsky_from_FITS.jpg')
+
+# meta_fermi = HipsTileMeta(order=3, ipix=-1, file_format='jpg')
+# allsky_fermi = HipsTileAllskyArray.read(meta_jpg, '../hips-extra/datasets/samples/FermiColor/Norder3/Allsky.jpg')
+# tile = allsky_fermi.tile(451)
+# tile.write('test_tile.jpg')
+#
+# print(allsky_fits.data.shape)
