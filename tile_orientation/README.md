db38243760b01a60c94040c5e7c7f1da16a90b3a
Christoph Deil <Deil.Christoph@gmail.com>
2017-07-24 15:02:43 +0200

    update

--- tile_orientation/README.md
+++ tile_orientation/README.md
@@ -3,14 +3,31 @@
 This script and images show that both HiPS tiles and HiPS all-sky files 
 are up-down flipped in FITS format when compared to the JPEG and PNG format.
 
-### Original all-sky image in JPG format
+## Tile image example
+
+### Original image in JPG format
+
+<img src="https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3//Dir0/Npix271.jpg" width="400">
+
+### From JPG format from the script
+
+![Tile_from_JPG.jpg](Tile_from_JPG.jpg)
+
+### From FITS format from the script
+
+![Tile_from_FITS.jpg](Tile_from_FITS.jpg)
+
+
+## All-sky image example
+
+### Original image in JPG format
 
 <img src="https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.jpg" width="400">
 
-### All-sky image in JPG format from the script
+### From JPG format from the script
 
 ![Allsky_from_JPG.jpg](Allsky_from_JPG.jpg)
 
-### All-sky image in FITS format from the script
+### From FITS format from the script
 
 ![Allsky_from_FITS.jpg](Allsky_from_FITS.jpg)
--- tile_orientation/tile_orientation.py
+++ tile_orientation/tile_orientation.py
@@ -11,9 +11,16 @@ data = allsky.data.astype('float')
 norm = simple_norm(data, 'linear', min_cut=0, max_cut=data.max())
 plt.figure()
 plt.imshow(data, origin='lower', norm=norm)
-plt.colorbar()
 plt.tight_layout()
 plt.savefig('Allsky_from_JPG.jpg')
+
+tile = allsky.tile(271)
+data = tile.data
+norm = simple_norm(data, 'linear', min_cut=0, max_cut=data.max())
+plt.figure()
+plt.imshow(data, origin='lower', norm=norm)
+plt.tight_layout()
+plt.savefig('Tile_from_JPG.jpg')
 
 meta_fits = HipsTileMeta(order=3, ipix=497, file_format='fits')
 url = 'https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.fits'
@@ -22,13 +29,13 @@ data = allsky.data.astype('float')
 norm = simple_norm(data, 'log', min_cut=0, max_cut=data.max())
 plt.figure()
 plt.imshow(data, origin='lower', norm=norm)
-plt.colorbar()
 plt.tight_layout()
 plt.savefig('Allsky_from_FITS.jpg')
 
-# meta_fermi = HipsTileMeta(order=3, ipix=-1, file_format='jpg')
-# allsky_fermi = HipsTileAllskyArray.read(meta_jpg, '../hips-extra/datasets/samples/FermiColor/Norder3/Allsky.jpg')
-# tile = allsky_fermi.tile(451)
-# tile.write('test_tile.jpg')
-#
-# print(allsky_fits.data.shape)
+tile = allsky.tile(271)
+data = tile.data
+norm = simple_norm(data, 'linear', min_cut=0, max_cut=data.max())
+plt.figure()
+plt.imshow(data, origin='lower', norm=norm)
+plt.tight_layout()
+plt.savefig('Tile_from_FITS.jpg')
