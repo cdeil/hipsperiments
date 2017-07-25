079c09f00815917a0e4f7f68b3fdaa71fc776ea7
Christoph Deil <Deil.Christoph@gmail.com>
2017-07-25 10:57:48 +0200

    update

--- tile_orientation/tile_orientation.py
+++ tile_orientation/tile_orientation.py
@@ -1,8 +1,38 @@
+import numpy as np
 import matplotlib
 matplotlib.use('agg')
 from hips import HipsTileMeta, HipsTileAllskyArray
 import matplotlib.pyplot as plt
 from astropy.visualization.mpl_normalize import simple_norm
+
+meta_fits = HipsTileMeta(order=3, ipix=497, file_format='fits')
+url = 'https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.fits'
+allsky = HipsTileAllskyArray.fetch(meta_fits, url)
+data = allsky.data.astype('float')
+norm = simple_norm(data, 'log', min_cut=0, max_cut=data.max())
+plt.figure()
+plt.imshow(data, norm=norm, origin='lower')
+plt.tight_layout()
+plt.savefig('Allsky_from_FITS.jpg')
+
+tile = allsky.tile(271)
+data = tile.data
+norm = simple_norm(data, 'linear', min_cut=0, max_cut=data.max())
+plt.figure()
+plt.imshow(data, norm=norm, origin='lower')
+plt.tight_layout()
+plt.savefig('Tile_from_FITS.jpg')
+
+
+# Print infos about the brightest pixel in the FITS image
+max_value = allsky.data.max()
+idx = np.where(allsky.data == max_value)
+print('Brightest pixel value:', max_value)
+print('Brightest pixel in allsky data:', idx)
+for tile in allsky.tiles:
+    idx = np.where(tile.data == max_value)
+    if len(idx[0]) != 0:
+        print(f'Brightest pixel in tile {tile.meta.ipix} data:', idx)
 
 meta_jpg = HipsTileMeta(order=3, ipix=497, file_format='jpg')
 url = 'https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.jpg'
@@ -22,20 +52,3 @@ plt.imshow(data, norm=norm, origin='lower')
 plt.tight_layout()
 plt.savefig('Tile_from_JPG.jpg')
 
-meta_fits = HipsTileMeta(order=3, ipix=497, file_format='fits')
-url = 'https://github.com/hipspy/hips-extra/raw/master/datasets/samples/IRAC4/Norder3/Allsky.fits'
-allsky = HipsTileAllskyArray.fetch(meta_fits, url)
-data = allsky.data.astype('float')
-norm = simple_norm(data, 'log', min_cut=0, max_cut=data.max())
-plt.figure()
-plt.imshow(data, norm=norm, origin='lower')
-plt.tight_layout()
-plt.savefig('Allsky_from_FITS.jpg')
-
-tile = allsky.tile(271)
-data = tile.data
-norm = simple_norm(data, 'linear', min_cut=0, max_cut=data.max())
-plt.figure()
-plt.imshow(data, norm=norm, origin='lower')
-plt.tight_layout()
-plt.savefig('Tile_from_FITS.jpg')
